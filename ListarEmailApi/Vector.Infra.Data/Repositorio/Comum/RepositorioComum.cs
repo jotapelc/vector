0a22f793a51ff5786ef42c4881b0023e67bb7807
DanPirs <danilopiresilva122@gmail.com>
2022-02-09 09:31:26 -0300

    Inclusão de classe teste

---
+++ ListarEmailApi/Vector.Infra.Data/Repositorio/Comum/RepositorioComum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector.Infra.Data.Repositorio.Comum
+{
+    public class RepositorioComum
+    {
+    }
+}
